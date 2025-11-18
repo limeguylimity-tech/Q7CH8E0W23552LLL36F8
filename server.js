cf0e75996d8b5d39baba8ee7650389c9bd5aaff9
limeguylimity-tech <limeguylimity@gmail.com>
2025-11-18 14:03:48 -0800

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+});
 
 // Database connection
 const pool = new Pool({
